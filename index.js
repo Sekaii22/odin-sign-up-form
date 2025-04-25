652ce316511e7bbe6a7a5a68d21e7cb16b5afad6
Sekaii22 <shawn.ng22@gmail.com>
2025-04-25 19:49:02 +0800

    Fix: Bug where non-matching password can be submitted
    Bug where typing the same password at first on both sides
    then change it on the password side will let non-matching values
    to be submitted.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const pwdInput = document.querySelector("#pwd");
 const pwdConfirmInput = document.querySelector("#pwd-confirm");
+const form = document.querySelector("form");
 
 pwdConfirmInput.addEventListener("input", (e) => {
     if (pwdInput.value !== pwdConfirmInput.value) {
-        pwdConfirmInput.setCustomValidity("Password not the same");
+        pwdConfirmInput.setCustomValidity("Password do not match.");
     }
     else {
         pwdConfirmInput.setCustomValidity("");
+    }
+});
+
+pwdInput.addEventListener("input", (e) => {
+    if (pwdInput.value !== pwdConfirmInput.value) {
+        pwdConfirmInput.setCustomValidity("Password do not match.");
+    }
+    else {
+        pwdConfirmInput.setCustomValidity("");
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    if (pwdInput.value !== pwdConfirmInput.value) {
+        pwdConfirmInput.setCustomValidity("Password do not match.");
+        e.preventDefault();
     }
 });
